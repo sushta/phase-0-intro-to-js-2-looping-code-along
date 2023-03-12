11bab4caeb80415eec525f853f42fe138148f623
sustapakhreen <sushta48@gmail.com>
2023-03-11 22:57:14 -0800

    Complete Loop Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const names =["Guadalupe", "Ollie", "Aki"];
+
+
+
+function writeCards() {
+  const messages = [];
+  for (let i = 0; i <names.length; i++){
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(message);
+  }
+  return messages;
+  
+}
+
+function countDown() {
+for (let countdown = 0; countdown < 11; countdown++) {
+  console.log(countdown);
+}
+}
